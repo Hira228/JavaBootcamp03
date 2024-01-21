850a2bfb1bcdce72dc22cc3d607a9d07259ca0fd
Grady Zankale <gradyzan@at-g1.msk.21-school.ru>
2024-01-21 17:53:05 +0300

    sdf

--- src/ex02/ex02.java
+++ src/ex02/ex02.java
@@ -3,24 +3,26 @@ import java.util.Random;
 import java.util.stream.IntStream;
 
 public class ex02 {
-    private static int sumArray = 0;
+    private static long sumArray = 0;
 
     public static void main(String[] args) throws InterruptedException {
-
         int[] arr = new int[Integer.parseInt(args[0])];
         int numberOfThreads = Integer.parseInt(args[1]);
         Random random = new Random();
         IntStream.range(0, arr.length)
                 .forEach(i -> arr[i] = random.nextInt(100));
 
+        System.out.println("Sum: " + Arrays.stream(arr).sum());
+
         Thread[] threads = new Thread[numberOfThreads];
 
         for (int i = 0; i < numberOfThreads; i++) {
             final int start = i * arr.length / numberOfThreads;
             final int end = (i + 1) * arr.length / numberOfThreads;
+            final int num = i;
 
             threads[i] = new Thread(() -> {
-                sumArr(start, end, arr);
+                sumArr(start, end, arr, num);
             });
             threads[i].start();
         }
@@ -29,14 +31,14 @@ public class ex02 {
             thread.join();
         }
 
-        System.out.println(Arrays.stream(arr).sum());
-        System.out.println(sumArray);
+        System.out.println("Sum by threads: " + sumArray);
 
     }
 
-    private synchronized static void sumArr(int a, int b, int[] arr) {
+    private synchronized static void sumArr(int a, int b, int[] arr, int num) {
         for(int i = a; i != b; ++i){
             sumArray += arr[i];
         }
+        System.out.println("Thread " + num + ": from " + a + " to " + b + " sum is " + (a + b));
     }
 }
